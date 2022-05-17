600a9b6f9a4ffbf2cd084a77b6c3594fe53eba76
Mark-Kohrman <mark.kohrman@gmail.com>
2022-05-16 20:51:08 -0500

    add components, props

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -10,7 +10,7 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+  <x-card class="p-10">
     <div
         class="flex flex-col items-center justify-center text-center"
     >
@@ -75,7 +75,8 @@
             </div>
         </div>
     </div>
-</div>
+  </x-card>
 </div>
 @endif
 @endsection
+ 
